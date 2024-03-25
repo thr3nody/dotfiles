95cfb528c89a762593074430ee806d4feb4703ed
thr3nody <devitousers@gmail.com>
2024-03-26 00:39:42 +0700

    fish 2.0

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,45 +1,17 @@
 #
 # very simple
 # config.fish by erine@archangels
-# version 0.1.7
+# version 0.2.0
 #
 # Changelog :
-# Added aliases for <neofetch> for easier image backend usage. Configure the one I marked as  NOTE:
+# Better support for Debian based Distro.
 #
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    # WARNING: No no no don't do anything here (yet(?)):D
-end
-
+# TODO: Fish functions
+#
 # Greeting function
 function fish_greeting
-    # Fish greetin ghere
-end
-
-# Random function
-function hi
-    figlet Hello
-end
-
-# warp-cli use case functions
-function warp-start
-    echo 'Starting Warp Daemon'
-    # WARNING: SUDO command below
-    sudo systemctl start warp-svc
-    echo 'Proceed with <warp-conn> to connect to Warp.'
-end
-
-function warp-conn
-    echo 'Connecting to Warp: '
-    warp-cli connect
-    warp-cli status
-
-end
-
-function warp-exit
-    echo 'Disconnecting from Warp: '
-    warp-cli disconnect
+    # Fish greeting here
 end
 
 # Spotify interface function
@@ -48,7 +20,7 @@ function s
         echo 'Spotify Interface undefined/not found.'
         echo 'Set $spotify user interface variable before using this command,'
         echo "To do that, use [set -U spotify '<spotify-ui>'] to set it up globally,"
-        echo "Or use [set -U spotify '<spotify-ui>'] to set it up for current terminal session."
+        echo "Or use [set spotify '<spotify-ui>'] to set it up for current terminal session."
         echo 'Change the <spotify-ui> to any desired Spotify interface like spotify-launcher, spt(spotify-tui), ncspot, etc.'
     else
         echo "Using $spotify"
@@ -63,11 +35,32 @@ function updt
         sudo pacman -Syu
     else if cat /etc/os-release | grep ID="ubuntu" -q
         sudo apt update && sudo apt upgrade
+    else
+        echo "Current OS/Linux Distro is not yet supported for this command."
+        echo "Add your own command into the fish configuration or just talk to Erine."
     end
 end
 
+# Function for downloading all requirements for this fish configuration
+function fishing
+    if cat /etc/os-release | grep ID="arch" -q
+        sudo pacman -S --needed zoxide fd fzf tmux starship
+    else
+        echo "Current OS/Linux Distro is not yet supported for this command."
+        echo "Add your own command into the fish configuration or just talk to Erine."
+    end
+end
 
-# Aliases
+# Configure fish function
+function fishc
+    # Comment/Uncomment, OR, just edit the path based on your fish configuration path
+    # v ~/.config/fish/
+    v ~/dotfiles/.config/fish/
+end
+
+# TODO: Aliases
+#
+# Shell aliases for fish
 alias d="dolphin"
 alias v="nvim"
 alias vh="nvim ."
@@ -75,16 +68,10 @@ alias c="code"
 alias ch="code ."
 alias gpu="nvidia-smi"
 alias kitth="kitten theme"
-alias pokemon="pokemon-colorscripts -r"
 
-#
-# NOTE: Comment/Uncomment for Neofetch with custom img and sources, modify as needed.
-# As for me, I like it to automatically clear the terminal as well so.
-# For the <fetch> one, I think maybe because the use of [chafa] and img backend, it will automatically clears the terminal.
-# Any fix for that please come to me.
-#
+# alias pokemon="pokemon-colorscripts -r"
 # alias neofetch="neofetch --ascii --source ~/.config/neofetch/rose.txt"
-alias fetch="neofetch --chafa --size 285px --source ~/Pictures/4Chan/Evangelion/lonelyBig.jpg"
+# alias fetch="neofetch --chafa --size 285px --source ~/Pictures/4Chan/Evangelion/lonelyBig.jpg"
 
 alias sl="fd --hidden | fzf-tmux -p | xargs nvim"
 alias sf="fd --hidden --type f | fzf-tmux -p | xargs nvim"
@@ -92,41 +79,39 @@ alias sd="fd --hidden --type d | fzf-tmux -p | xargs nvim"
 
 alias q="exit"
 
+alias uwu="sudo"
+
+# alias minecraft="prime-run java -jar ~/lLauncher/LegacyLauncher_legacy.jar"
+
+# Arch Based aliases
 alias pacclr="sudo pacman -Sc"
 alias pacclr-full="sudo pacman -Scc"
+
 alias yayclr="yay -Sc"
 alias yayclr-full="yay -Scc"
 
-alias fuck="sudo pacman -S"
-alias uwu="sudo"
+alias fuck="sudo pacman -S --needed"
 
-alias minecraft="prime-run java -jar ~/lLauncher/LegacyLauncher_legacy.jar"
-
-# Fish functions
-function fishc
-    cd ~/.config/fish/
-    vh
-end
-
-# TODO: All documentations go here
+# TODO: Documentations
+#
+# help-like command for this configuration
 function fishh
     echo '''
 fishh               --Show all Fish commands from config.fish
 fishc               --Configure Fish
+fishing             --Install everything that is needed for this fish config
 
 d                   --Dolphin
 v                   --NeoVim
 vh                  --NeoVim in current directory
 c                   --Code OSS
 ch                  --Code OSS in current directory
-s                   --Spotify interface.
+s                   --Spotify interface
 nc                  --Ncspot
 gpu                 --nvidia-smi
 kitth               --Kitten themes
-pekemon             --Summon random pokemon
 
-fetch               --Neofetch with custom img-backend
-neofetch            --Neofetch with custom ascii art
+updt                --Full system update
 
 sl                  --Advanced search system for both files and directories.
 sf                  --Advanced search system for files only.
@@ -134,25 +119,20 @@ sd                  --advanced search system for directories only.
 
 q                   --Exit terminal
 
-updt                --Full system update
+uwu                 --Alias for sudo
 
 pacclr              --Pacman packages and caches cleanup
 pacclr-full         --Aggresive Pacman packages and caches cleanup
+
 yayclr              --Yay packages and caches cleanup
 yayclr-full         --Aggresive Yay packages and caches cleanup
 
-warp-start          --Start walp-cli daemon, use <warp-conn>
-                      to connect to Warp after daemon started
-warp-conn           --Connect to Warp
-warp-exit           --Disconnect from Warp
-
-minecraft           --Shortcut for using nvidia <prime-run> to run minecraft launcher.
-
 fuck <package_name> --Pacman package install
-uwu                 --Alias for sudo
 '''
 end
 
+# TODO: Fish inits
+#
 # Starship Terminal Styling
 starship init fish | source
 # Zoxide Better <cd> Command
---
+++ README.md
@@ -0,0 +1,13 @@
+# Erine's dotfiles
+
+This is collection of dotfiles made with Stow.
+
+## Usage
+
+To use this dotfiles repo with Stow, clone this repo to your $HOME directory, then use Stow to create symlinks to the $HOME/.config/ directory.
+
+```
+git clone https://github.com/thr3nody/dotfiles.git
+cd dotfiles
+stow .
+```
