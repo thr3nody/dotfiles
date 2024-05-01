7aa855dc2859431e6a1610e567d3bd4650fa6c25
thr3nody <devitousers@gmail.com>
2024-05-02 00:39:35 +0700

    Remove <apt install starship> in <fishing>

--- .config/fish/functions/fishing.fish
+++ .config/fish/functions/fishing.fish
@@ -3,7 +3,7 @@ function fishing
     if test -e /usr/bin/pacman
         sudo pacman -S --needed neovim zoxide fd fzf tmux starship
     else if test -e /usr/bin/apt
-        sudo apt install neovim zoxide fd-find fzf tmux starship
+        sudo apt install neovim zoxide fd-find fzf tmux
         curl -sS https://starship.rs/install.sh | sh
     else
         echo "Current package manager is either not found or not yet supported for this command."
