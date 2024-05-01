09e61f64df32790a4f414db6c1776d701f174ad5
thr3nody <devitousers@gmail.com>
2024-05-02 00:50:34 +0700

    Add messages for transparency in <updt>.

--- .config/fish/functions/updt.fish
+++ .config/fish/functions/updt.fish
@@ -1,14 +1,23 @@
 # System update function
 function updt
+echo "Starting a full system update."
     if test -e /usr/bin/pacman
         if test -e /usr/bin/eos-update
+            echo "[eos-update] detected in the system."
+            echo "Doing: eos-update --aur"
             eos-update --aur
         else if test -e /usr/bin/yay
-            yay -Syu
+            echo "[yay] detected in the system."
+            echo "Doing: yay"
+            yay
         else
+            echo "[pacman] detected in the system."
+            echo "Doing: yay"
             sudo pacman -Syu
         end
     else if test -e /usr/bin/apt
+            echo "[apt] detected in the system."
+            echo "Doing: sudo apt update && sudo apt upgrade"
         sudo apt update && sudo apt upgrade
     else
         echo "Current package manager is either not found or not yet supported for this command."
